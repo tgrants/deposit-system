45a11258e20bbbca18b1f38ee80f07665deffbe7
Toms Grants <75024514+tgrants@users.noreply.github.com>
2024-10-31 01:42:00 +0200

    Motor control (#9)
    * Add motor control
    * Add persistent storage
    * Fix high CPU usage

--- driver/driver.ino
+++ driver/driver.ino
@@ -1,24 +1,58 @@
 #include "Arduino.h"
+#include <EEPROM.h>
+#include <Stepper.h>
 #include <Vrekrer_scpi_parser.h>
 
-SCPI_Parser scpi;
+// Motor
+const int dirPin = 8;
+const int stepPin = 9;
+const int stepsPerRevolution = 200;
+Stepper stepper(stepsPerRevolution, dirPin, stepPin);
+long stepDelay = 1000;
 
-const int ledPin = LED_BUILTIN;
+// Serial
+#define BAUD 9600
+
+// State
+const int lockPositionAddress = 0;
+const int lockLockedPosition = 800;
+int lockPosition = 0;
+
+// SCPI
+SCPI_Parser scpi;
 
 void setup() {
 	scpi.RegisterCommand("*IDN?", &identify);
 	scpi.SetCommandTreeBase(F("LED"));
 		scpi.RegisterCommand(F(":ON"), &ledOn);
 		scpi.RegisterCommand(F(":OFF"), &ledOff);
+	scpi.SetCommandTreeBase(F("LOCK"));
+		scpi.RegisterCommand(F(":ON"), &lockOn);
+		scpi.RegisterCommand(F(":OFF"), &lockOff);
  
-	Serial.begin(9600);
-	pinMode(ledPin, OUTPUT);
+	Serial.begin(BAUD);
+	pinMode(LED_BUILTIN, OUTPUT);
+	EEPROM.get(lockPositionAddress, lockPosition);
 }
 
 void loop() {
 	if (Serial.available()) {
 		scpi.ProcessInput(Serial, "\n");
 	}
+}
+
+void pause(long ms) {
+	delay(ms / 1000);
+	delayMicroseconds(ms % 1000);
+}
+
+void moveMotor(int steps, int dir) {
+	for (int i = 0; i < steps; ++i) {
+		stepper.step(dir);
+		lockPosition += dir;
+		pause(stepDelay);
+	}
+	EEPROM.put(lockPositionAddress, lockPosition);
 }
 
 void identify(SCPI_C commands, SCPI_P parameters, Stream& interface) {
@@ -28,9 +62,17 @@ void identify(SCPI_C commands, SCPI_P parameters, Stream& interface) {
 }
 
 void ledOn(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-	digitalWrite(ledPin, HIGH);
+	digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void ledOff(SCPI_C commands, SCPI_P parameters, Stream& interface) {
-	digitalWrite(ledPin, LOW);
+	digitalWrite(LED_BUILTIN, LOW);
+}
+
+void lockOn(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	moveMotor(lockLockedPosition - lockPosition, 1);
+}
+
+void lockOff(SCPI_C commands, SCPI_P parameters, Stream& interface) {
+	moveMotor(lockPosition, -1);
 }
